22b2ecd8619d14288f115fa4e091167aa1bda5e9
Kelsey Kruzona <kelsey@fusionary.com>
2021-12-20 16:16:58 -0500

    basically done

--- script.js
+++ script.js
@@ -1,31 +1,13 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+{/* <script>
+    window.onscroll = function() {scrollFunction()};
+ 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    
+    document.getElementById("navbar").style.background = "#501e27";
   } else {
-    navbar.classList.remove("sticky");
+   
+    document.getElementById("navbar").style.background = "none";
   }
 }
-
-
-// $(document).ready(function(){
-//     $(window).scroll(function(){
-//         var scroll = $(window).scrollTop();
-//         if (scroll > 300) {
-//           $(".mainNav").css("background" , "white");
-//         }
-  
-//         else{
-//             $(".mainNav").css("background" , "#ffffff");  	
-//         }
-//     })
-//   })
+</script> */}
